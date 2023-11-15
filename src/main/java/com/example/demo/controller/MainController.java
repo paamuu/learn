0cb3098979ba37fb21ce5b42cc08ab8a7f8179df
papa <xzy58188@163.com>
2023-11-15 19:34:39 +0800

    静态文件映射

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,13 +1,26 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.City;
+import com.example.demo.mapper.CityMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MainController {
+    @Autowired
+    CityMapper cityMapper;
 
     @GetMapping("/hello")
     public String hello() {
         return "hello worldddd";
+    }
+
+    @RequestMapping("/cities")
+    public List<City> getCity() {
+        return cityMapper.getAllCity();
     }
 }
---
+++ src/main/java/com/example/demo/entity/City.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+@Data
+public class City {
+    private String city_id;
+    private String city;
+    private String country_id;
+    private String last_update;
+}
